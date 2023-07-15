2daddac5a618bb1704b86e140fa8f1da2b186601
zhouxing <2579847301@qq.com>
2023-07-15 15:26:15 +0800

    day1-LRU 缓存淘汰策略

--- day1-lru/geecache/lru/lru_test.go
+++ day1-lru/geecache/lru/lru_test.go
@@ -1,1 +1,66 @@
 package lru
+
+import (
+	"reflect"
+	"testing"
+)
+
+type String string
+
+func (d String) Len() int {
+	return len(d)
+}
+
+func TestGet(t *testing.T) {
+	lru := New(int64(0), nil)
+	//在这个特定的上下文中，int64(0) 作为参数传递给 New 函数，用于指定 LRU 缓存的最大存储容量。
+	//在这里，将其设置为 0 表示缓存的最大容量为零，即没有存储空间，因此不会保存任何键值对。
+	//这可以用于创建一个非常小的缓存或用于特定的测试场景，其中不需要实际存储数据。
+	lru.Add("key1", String("1234"))
+	if v, ok := lru.Get("key1"); !ok || string(v.(String)) != "1234" {
+		t.Fatalf("cache hit key1=1234 failed")
+	}
+	if _, ok := lru.Get("key2"); ok {
+		t.Fatalf("cache miss key2 failed")
+	}
+}
+
+func TestRemoveoldest(t *testing.T) {
+	k1, k2, k3 := "key1", "key2", "k3"
+	v1, v2, v3 := "value1", "value2", "v3"
+	Cap := len(k1 + k2 + v1 + v2)
+	lru := New(int64(Cap), nil)
+	lru.Add(k1, String(v1))
+	lru.Add(k2, String(v2))
+	lru.Add(k3, String(v3))
+
+	if _, ok := lru.Get("key1"); ok || lru.Len() != 2 {
+		t.Fatalf("Removeoldest key1 failed")
+	}
+}
+
+func TestOnEvicted(t *testing.T) {
+	keys := make([]string, 0)
+	callback := func(key string, value Value) {
+		keys = append(keys, key)
+	}
+	lru := New(int64(10), callback)
+	lru.Add("key1", String("123456"))
+	lru.Add("k2", String("k2"))
+	lru.Add("k3", String("k3"))
+	lru.Add("k4", String("k4"))
+	expect := []string{"key1", "k2"}
+	if !reflect.DeepEqual(expect, keys) {
+		t.Fatalf("Call onEvicted failed,expect keys equals to %s", expect)
+	}
+}
+
+func TestAdd(t *testing.T) {
+	lru := New(int64(0), nil)
+	lru.Add("key", String("1"))
+	lru.Add("key", String("111"))
+
+	if lru.nBytes != int64(len("key")+len("111")) {
+		t.Fatal("expected 6 but got", lru.nBytes)
+	}
+}
