706bfbeb4a268768d1a99ba4411bd5cdd86fa9c0
zhouxing <2579847301@qq.com>
2023-07-16 01:29:48 +0800

    一致性哈希

--- geecache/consistenthash/consistenthash.go
+++ geecache/consistenthash/consistenthash.go
@@ -6,14 +6,18 @@ import (
 	"strconv"
 )
 
-type Hash func(data []byte) uint32
+type Hash func(data []byte) uint32 //定义了函数类型 Hash，采取依赖注入的方式，允许用于替换成自定义的 Hash 函数，也方便测试时替换，默认为 crc32.ChecksumIEEE 算法。
 
 type Map struct {
 	hash     Hash
 	replicas int
 	keys     []int
 	hashMap  map[int]string
-}
+} //Map 是一致性哈希算法的主数据结构，包含 4 个成员变量：
+//Hash 函数 hash；
+//虚拟节点倍数 replicas；
+// 哈希环 keys；
+//虚拟节点与真实节点的映射表 hashMap，键是虚拟节点的哈希值，值是真实节点的名称。
 
 func New(replicas int, fn Hash) *Map {
 	m := &Map{
@@ -36,7 +40,11 @@ func (m *Map) Add(keys ...string) {
 		}
 	}
 	sort.Ints(m.keys)
-}
+} //Add 函数允许传入 0 或 多个真实节点的名称。
+//对每一个真实节点 key，对应创建 m.replicas 个虚拟节点，虚拟节点的名称是：strconv.Itoa(i) + key，即通过添加编号的方式区分不同虚拟节点。
+//使用 m.hash() 计算虚拟节点的哈希值，使用 append(m.keys, hash) 添加到环上。
+//在 hashMap 中增加虚拟节点和真实节点的映射关系。
+//最后一步，环上的哈希值排序。
 
 func (m *Map) Get(key string) string {
 	if len(m.keys) == 0 {
@@ -47,4 +55,7 @@ func (m *Map) Get(key string) string {
 		return m.keys[i] >= hash
 	})
 	return m.hashMap[m.keys[idx%len(m.keys)]]
-}
+} //选择节点就非常简单了，第一步，计算 key 的哈希值。
+//第二步，顺时针找到第一个匹配的虚拟节点的下标 idx，从 m.keys 中获取到对应的哈希值。
+//如果 idx == len(m.keys)，说明应选择 m.keys[0]，因为 m.keys 是一个环状结构，所以用取余数的方式来处理这种情况。
+//第三步，通过 hashMap 映射得到真实的节点。
