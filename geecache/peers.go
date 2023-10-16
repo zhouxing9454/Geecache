8b3aeef1cd7f2b406cb5146276400f71379649b6
zhouxing <2579847301@qq.com>
2023-10-17 00:28:19 +0800

    注释修改

--- geecache/peers.go
+++ geecache/peers.go
@@ -11,4 +11,4 @@ type PeerGetter interface {
 }
 
 //在这里，抽象出 2 个接口，PeerPicker 的 PickPeer() 方法用于根据传入的 key 选择相应节点 PeerGetter。
-//接口 PeerGetter 的 Get() 方法用于从对应 group 查找缓存值。PeerGetter 就对应于上述流程中的 HTTP 客户端。
+//接口 PeerGetter 的 Get() 方法用于从对应 group 查找缓存值。PeerGetter 就对应于上述流程中相应远程节点的客户端。
