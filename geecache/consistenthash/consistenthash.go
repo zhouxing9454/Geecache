cb1cba7811bfa69e6af420bcd71aa7eac507d784
zhouxing <2579847301@qq.com>
2023-10-16 15:34:55 +0800

    注释修改

--- geecache/consistenthash/consistenthash.go
+++ geecache/consistenthash/consistenthash.go
@@ -8,17 +8,21 @@ import (
 
 type Hash func(data []byte) uint32 //定义了函数类型 Hash，采取依赖注入的方式，允许用于替换成自定义的 Hash 函数，也方便测试时替换，默认为 crc32.ChecksumIEEE 算法。
 
+/*
+Map 是一致性哈希算法的主数据结构，包含 4 个成员变量：
+Hash 函数 hash；
+虚拟节点倍数 replicas；
+哈希环 keys；
+虚拟节点与真实节点的映射表 hashMap，键是虚拟节点的哈希值，值是真实节点的名称。
+*/
 type Map struct {
 	hash     Hash
 	replicas int
 	keys     []int
 	hashMap  map[int]string
-} //Map 是一致性哈希算法的主数据结构，包含 4 个成员变量：
-//Hash 函数 hash；
-//虚拟节点倍数 replicas；
-// 哈希环 keys；
-//虚拟节点与真实节点的映射表 hashMap，键是虚拟节点的哈希值，值是真实节点的名称。
+}
 
+// New 函数通过传入的虚拟节点倍数replicas和哈希函数fn，返回一个名为Map的数据结构。
 func New(replicas int, fn Hash) *Map {
 	m := &Map{
 		replicas: replicas,
@@ -26,11 +30,18 @@ func New(replicas int, fn Hash) *Map {
 		hashMap:  make(map[int]string),
 	}
 	if m.hash == nil {
-		m.hash = crc32.ChecksumIEEE
+		m.hash = crc32.ChecksumIEEE //使用默认的哈希算法crc32.ChecksumIEEE
 	}
 	return m
 }
 
+/*
+Add 函数允许传入 0 或 多个真实节点的名称。
+对每一个真实节点 key，对应创建 m.replicas 个虚拟节点，虚拟节点的名称是：strconv.Itoa(i) + key，即通过添加编号的方式区分不同虚拟节点。
+使用 m.hash() 计算虚拟节点的哈希值，使用 append(m.keys, hash) 添加到环上。
+在 hashMap 中增加虚拟节点和真实节点的映射关系。
+最后一步，环上的哈希值排序。
+*/
 func (m *Map) Add(keys ...string) {
 	for _, key := range keys {
 		for i := 0; i < m.replicas; i++ {
@@ -40,12 +51,15 @@ func (m *Map) Add(keys ...string) {
 		}
 	}
 	sort.Ints(m.keys)
-} //Add 函数允许传入 0 或 多个真实节点的名称。
-//对每一个真实节点 key，对应创建 m.replicas 个虚拟节点，虚拟节点的名称是：strconv.Itoa(i) + key，即通过添加编号的方式区分不同虚拟节点。
-//使用 m.hash() 计算虚拟节点的哈希值，使用 append(m.keys, hash) 添加到环上。
-//在 hashMap 中增加虚拟节点和真实节点的映射关系。
-//最后一步，环上的哈希值排序。
+}
 
+/*
+Get 函数主要是通过key获取真实节点
+第一步，计算 key 的哈希值。
+第二步，顺时针找到第一个匹配的虚拟节点的下标 idx，从 m.keys 中获取到对应的哈希值。
+如果 idx == len(m.keys)，说明应选择 m.keys[0]，因为 m.keys 是一个环状结构，所以用取余数的方式来处理这种情况。
+第三步，通过 hashMap 映射得到真实的节点。
+*/
 func (m *Map) Get(key string) string {
 	if len(m.keys) == 0 {
 		return ""
@@ -55,7 +69,4 @@ func (m *Map) Get(key string) string {
 		return m.keys[i] >= hash
 	})
 	return m.hashMap[m.keys[idx%len(m.keys)]]
-} //选择节点就非常简单了，第一步，计算 key 的哈希值。
-//第二步，顺时针找到第一个匹配的虚拟节点的下标 idx，从 m.keys 中获取到对应的哈希值。
-//如果 idx == len(m.keys)，说明应选择 m.keys[0]，因为 m.keys 是一个环状结构，所以用取余数的方式来处理这种情况。
-//第三步，通过 hashMap 映射得到真实的节点。
+}
